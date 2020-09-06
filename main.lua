34a18c3ab9a1945c91bd9e9b8ab9e19413af1037
LudiusMaximus <4678285+LudiusMaximus@users.noreply.github.com>
2020-09-06 14:31:17 +0200

    Map now automatically switches to new zone; also while shown. Unless the player has been looking at another map than the previously current zone.

--- main.lua
+++ main.lua
@@ -14,10 +14,10 @@ local lastScrollY = nil
 local lastMapCloseTime = GetTime()
 local resetMapAfter = 15
 
--- Store if the last viewed map was the player's current map.
--- If not we shall only reset the lastMapID when changing to
--- a whole new area.
-local lastMapWasCurrentMap = true
+-- If the last viewed map was the map in which the player was,
+-- we want the map to automatically change to the new map;
+-- both if the map is visible or not.
+local lastViewedMapWasCurrentMap = false
 
 
 local function SafeMapState()
@@ -26,17 +26,12 @@ local function SafeMapState()
   lastScrollX = WorldMapFrame.ScrollContainer.currentScrollX
   lastScrollY = WorldMapFrame.ScrollContainer.currentScrollY
   -- print("saving", lastMapID, lastScale, lastScrollX, lastScrollY)
-
-  if lastMapID == MapUtil.GetDisplayableMapForPlayer() then
-    lastMapWasCurrentMap = true
-  else
-    lastMapWasCurrentMap = false
-  end
 end
 
 
+-- Called when reopenning the map.
 local function RestoreMapState()
-  -- print("trying", lastMapID, lastScale, lastScrollX, lastScrollY)
+  -- print("RestoreMapState", lastMapID, lastScale, lastScrollX, lastScrollY)
   if lastMapID and lastScale and lastScrollX and lastScrollY then
     -- print("restoring", lastMapID, lastScale, lastScrollX, lastScrollY)
 
@@ -65,9 +60,9 @@ hooksecurefunc("ToggleWorldMap", function()
     -- print("Post Hook after showing", GetTime(), lastMapCloseTime, GetTime() - lastMapCloseTime, resetMapAfter)
     if GetTime() - lastMapCloseTime > resetMapAfter then
       lastMapID = nil
+    else
+      RestoreMapState()
     end
-
-    RestoreMapState()
   end
 end)
 
@@ -82,8 +77,6 @@ WorldMapFrame.ScrollContainer:SetScript("OnHide", function(self, ...)
   SafeMapState()
   OtherWorldMapFrameOnHideScripts(self, ...)
 end)
-
-
 
 -- Also got to store and restore when the SidePanelToggle is shown or hidden.
 local OtherCloseButtonScripts = WorldMapFrame.SidePanelToggle.CloseButton:GetScript("OnClick")
@@ -105,9 +98,15 @@ local function ResetMap()
   WorldMapFrame:SetMapID(MapUtil.GetDisplayableMapForPlayer())
   local currentScale = WorldMapFrame.ScrollContainer:GetCanvasScale()
   local currentZoomLevel = WorldMapFrame.ScrollContainer:GetZoomLevelIndexForScale(currentScale)
+
+  -- Got to do this to avoid funny shift of the map by 1 pixel...
   if currentZoomLevel ~= 1 then
     WorldMapFrame:ResetZoom()
   end
+
+  SafeMapState()
+
+  WorldMapFrame:OnMapChanged()
 end
 
 
@@ -121,45 +120,26 @@ WorldMapFrame.ScrollContainer:HookScript("OnMouseDown", function(self)
     resetMap = false
   end
 end)
--- Got to do this here as well because otherwise the map of the cursor position gets
+-- Got to do this because otherwise the map of the cursor position gets
 -- loaded when releasing the mouse button.
 WorldMapFrame.ScrollContainer:HookScript("OnMouseUp", function(self)
   if resetMap then
-    WorldMapFrame:SetMapID(MapUtil.GetDisplayableMapForPlayer())
+    ResetMap()
     resetMap = false
   end
 end)
 
 
+
+
 resetButton = CreateFrame("Button", nil, WorldMapFrame.ScrollContainer, "UIPanelButtonTemplate")
-resetButton:SetPoint("BOTTOMRIGHT", WorldMapFrame.SidePanelToggle.CloseButton, "BOTTOMLEFT", -5, 5)
+resetButton:SetPoint("BOTTOMRIGHT", WorldMapFrame.SidePanelToggle.CloseButton, "BOTTOMLEFT", 1, 1)
 resetButton:SetText("Reset View")
 resetButton:SetWidth(120)
 resetButton:SetScript("OnClick", function()
     ResetMap()
 	end)
 resetButton:Hide()
-
-
-
-
-
-
--- Boss buttons
--- local ejPinClick = EncounterJournalPinMixin.OnClick
--- function EncounterJournalPinMixin:OnClick()
-
-  -- print("Tainted stuff")
-
-  -- if not InCombatLockdown() then
-    -- ejPinClick(self)
-  -- else
-    -- print("cannot do in combat lockdown")
-  -- end
-
--- end
-
-
 
 
 
@@ -186,10 +166,14 @@ end)
 
 hooksecurefunc(WorldMapFrame, "SetMapID", function(self, mapID)
     -- print("SetMapID", mapID, MapUtil.GetDisplayableMapForPlayer())
-    if mapID ~= MapUtil.GetDisplayableMapForPlayer() then
-      resetButton:Show()
-    else
-      resetButton:Hide()
+
+    if WorldMapFrame:IsShown() then
+      lastViewedMapWasCurrentMap = (mapID == MapUtil.GetDisplayableMapForPlayer())
+      if mapID ~= MapUtil.GetDisplayableMapForPlayer() then
+        resetButton:Show()
+      else
+        resetButton:Hide()
+      end
     end
 
 
@@ -229,31 +213,25 @@ hooksecurefunc(WorldMapFrame, "SetMapID", function(self, mapID)
 local zoneChangeFrame = CreateFrame ("Frame")
 
 -- But only do this for areas, not for sub-zone changes.
+zoneChangeFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 zoneChangeFrame:RegisterEvent("ZONE_CHANGED")
 zoneChangeFrame:RegisterEvent("ZONE_CHANGED_INDOORS")
 zoneChangeFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-zoneChangeFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+-- Needed for map changes in dungeons.
+zoneChangeFrame:RegisterEvent("AREA_POIS_UPDATED")
 
 zoneChangeFrame:SetScript("OnEvent", function(self, event, ...)
-  -- print(event, ":", GetZoneText(), GetSubZoneText(), WorldMapFrame:GetMapID(), MapUtil.GetDisplayableMapForPlayer(), lastMapWasCurrentMap)
+  -- print(event, ":", GetZoneText(), GetSubZoneText(), WorldMapFrame:GetMapID(), MapUtil.GetDisplayableMapForPlayer(), lastViewedMapWasCurrentMap)
 
-  if WorldMapFrame:GetMapID() ~= MapUtil.GetDisplayableMapForPlayer() then
-    resetButton:Show()
+  if lastViewedMapWasCurrentMap then
+    ResetMap()
   else
-    resetButton:Hide()
-  end
-
-  -- For zone changes we are not resetting the map, if the player was previously looking at a different map.
-  if not lastMapWasCurrentMap and event ~= "PLAYER_ENTERING_WORLD" then
-    return
-  else
-    -- Only reset the map if this zone's map is a different one.
-    if lastMapID ~= MapUtil.GetDisplayableMapForPlayer() then
-      -- print("Resetting")
-      lastMapID = nil
+    if WorldMapFrame:GetMapID() ~= MapUtil.GetDisplayableMapForPlayer() then
+      resetButton:Show()
+    else
+      resetButton:Hide()
     end
   end
-
 
 end)
 
